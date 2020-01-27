45f88f5a8232eef7f88a3991d9042b8035db079d
ygcjal <46641912+ygcjal@users.noreply.github.com>
2020-01-27 19:55:23 +0530

    Add files via upload

--- InfoEqual.java
+++ InfoEqual.java
@@ -4,13 +4,13 @@ class InfoEqual
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		String[] s = new String[5];
+		String[] s = new String[3];
 		System.out.println("Enter "+s.length+" Strings");
 		for(int i=0; i<s.length; i++)
 			s[i] = in.nextLine();
 		for(int i=0; i<s.length; i++)
 		{
-			if(s[i].equal("Info"))
+			if(s[i].equals("Info"))
 			{
 				System.out.println("String is provided");
 				break;
